42294f5d018e9d0a1da4fc0d6ee3508f83ea30be
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2023-10-09 14:32:45 +0200

    Initial commit

---
+++ Assets/model/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+	public Animator daveAnim;
+	public Animator dave2Anim;
+
+    // Start is called before the first frame update
+
+	
+    void Start()
+    {
+        daveAnim = GetComponent<Animator>();
+	dave2Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {	
+        if(Input.GetKey("q")){
+		daveAnim.Play("nod");
+	}
+	if(Input.GetKey("w")){
+		daveAnim.Play("shake");	
+	}
+	if(Input.GetKey("e")){
+		daveAnim.Play("shrug");	
+	}
+    }
+
+  
+
+}
